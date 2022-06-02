689d45e0dc165bc95131b2d122ab9b63f1add277
Marius Albeser <albeser.marius@gmail.com>
2022-06-02 16:20:57 +0200

    added function to slice word second half off depending on their length default slice at char 6 when word length >  11

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const input = document.getElementById("input");
 
 
 function displayOutputText() {
-    const updatedOutput = mergeFirstTwoCharsWithRest();
+    const updatedOutput = mergeFirstCharsWithRest();
     const txt = document.createElement("p");
     txt.innerHTML = updatedOutput.join(" ");
     document.getElementById("output").appendChild(txt);
@@ -13,14 +13,14 @@ function getFirstChars() {
     const updatedArr = [];
     textArr.forEach(el => {
         if(el !== " ")
-        updatedArr.push(getWordLength(el));
+        updatedArr.push(getWordsFirstHalf(el));
     });
     console.log(updatedArr)
     return updatedArr;
     
 }
 
-function getWordLength(el){
+function getWordsFirstHalf(el){
     const obj = {
         1 : el.slice(0,1),
         2 : el.slice(0,2),
@@ -33,21 +33,37 @@ function getWordLength(el){
         9 : el.slice(0,5),
         10 : el.slice(0,5)
     }
-    console.log(typeof(el));
     return obj[el.length] ?? el.slice(0,6);
 }
+
+function getWordsSecondHalf(el){
+    const obj = {
+        1 : el.slice(1),
+        2 : el.slice(2),
+        3 : el.slice(2),
+        4 : el.slice(2),
+        5 : el.slice(3),
+        6 : el.slice(3),
+        7 : el.slice(4),
+        8 : el.slice(4),
+        9 : el.slice(5),
+        10 : el.slice(5)
+    }
+    return obj[el.length] ?? el.slice(6);
+}
+
 
 function getRemainingChars() {
     const textArr = input.value.split(/(\s+)/);
     const updatedArr = [];
     textArr.forEach(el => {
         if(el !== " ")
-        updatedArr.push(el.slice(2));
+        updatedArr.push(getWordsSecondHalf(el));
     });
     return updatedArr;
 }
 
-function transFirstTwoCharsBold(){
+function transFirstCharsBold(){
     const arr = getFirstChars();
     const updatedArr = [];
     arr.forEach(el => { 
@@ -56,12 +72,12 @@ function transFirstTwoCharsBold(){
     return updatedArr;
 }
 
-function mergeFirstTwoCharsWithRest(){
-    const firstTwoArr = transFirstTwoCharsBold();
+function mergeFirstCharsWithRest(){
+    const firstHalfArr = transFirstCharsBold();
     const restArr = getRemainingChars();
     const updatedArr = [];
     for (let index = 0; index < restArr.length; index++) {
-        updatedArr.push(firstTwoArr[index] + restArr[index]);
+        updatedArr.push(firstHalfArr[index] + restArr[index]);
     }
     return updatedArr;
 }
